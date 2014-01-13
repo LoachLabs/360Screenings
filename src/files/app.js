d768aeb089afca34dade285f209c010de2593148
Rob Loach <robloach@gmail.com>
2014-01-12 20:27:53 -0500

    Add new window links on Press page - fixes #9

--- src/files/app.js
+++ src/files/app.js
@@ -1,63 +1,67 @@
-$(function() {
-  /**
-   * The Slideshow.
-   */
-  backgroundList = [
-    {
-      src: "/images/background1.jpg",
-      fade: 4000,
-    },
-    {
-      src: "/images/background2.jpg",
-      fade: 4000,
-    },
-    {
-      src: "/images/background3.jpg",
-      fade: 4000,
-    },
-    {
-      src: "/images/background4.jpg",
-      fade: 4000,
-    },
-    {
-      src: "/images/background5.jpg",
-      fade: 4000,
-    },
-    {
-      src: "/images/background6.jpg",
-      fade: 4000,
-    },
-    {
-      src: "/images/background7.jpg",
-      fade: 4000,
-    },
-    {
-      src: "/images/background8.jpg",
-      fade: 4000,
-    },
-    {
-      src: "/images/background9.jpg",
-      fade: 4000,
-    },
-    {
-      src: "/images/background10.jpg",
-      fade: 4000,
-    }
-  ];
+(function($) {
+  $(document).ready(function() {
 
-  // Randomize the order.
-  backgroundList.sort(function() {
-    return Math.round(Math.random()) - 0.5;
-  });
+    /**
+     * The Slideshow.
+     */
+    backgroundList = [
+      {
+        src: "/images/background1.jpg",
+        fade: 4000,
+      },
+      {
+        src: "/images/background2.jpg",
+        fade: 4000,
+      },
+      {
+        src: "/images/background3.jpg",
+        fade: 4000,
+      },
+      {
+        src: "/images/background4.jpg",
+        fade: 4000,
+      },
+      {
+        src: "/images/background5.jpg",
+        fade: 4000,
+      },
+      {
+        src: "/images/background6.jpg",
+        fade: 4000,
+      },
+      {
+        src: "/images/background7.jpg",
+        fade: 4000,
+      },
+      {
+        src: "/images/background8.jpg",
+        fade: 4000,
+      },
+      {
+        src: "/images/background9.jpg",
+        fade: 4000,
+      },
+      {
+        src: "/images/background10.jpg",
+        fade: 4000,
+      }
+    ];
 
-  // Display the slideshow.
-  $.vegas('slideshow', {
-    backgrounds: backgroundList,
-    delay: 10000
-  });
+    // Randomize the order.
+    backgroundList.sort(function() {
+      return Math.round(Math.random()) - 0.5;
+    });
 
-  // Set the overlay.
-  $.vegas('overlay', {
-    src: '/vendor/vegas/dist/overlays/06.png'
+    // Display the slideshow.
+    $.vegas('slideshow', {
+      backgrounds: backgroundList,
+      delay: 10000
+    });
+
+    // Set the overlay.
+    $.vegas('overlay', {
+      src: '/vendor/vegas/dist/overlays/06.png'
+    });
+
   });
-});
+})(jQuery);
